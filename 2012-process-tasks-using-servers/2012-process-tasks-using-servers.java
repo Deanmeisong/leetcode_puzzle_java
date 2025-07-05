929cae711ea1e4023971d204fda2417eb9eb1ac3
dean <deanmeisong@gmail.com>
2025-07-06 00:02:35 +1000

    Time: 234 ms (30.62%), Space: 77.3 MB (52.85%) - LeetHub

---
+++ 2012-process-tasks-using-servers/2012-process-tasks-using-servers.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] assignTasks(int[] servers, int[] tasks) {
+        int n = servers.length;
+        PriorityQueue<int[]> idle = new PriorityQueue<>((a,b)-> {
+            if(a[0] != b[0]) return a[0]-b[0];
+            return a[1]-b[1];
+            }
+        );
+
+        PriorityQueue<int[]> busy = new PriorityQueue<>((a,b)-> {
+                if(a[0] != b[0]) return a[0]-b[0];
+                if(a[1] != b[1]) return a[1]-b[1];
+                return a[2]-b[2];
+            }
+        );
+
+        for(int i = 0; i < n; ++i) {
+            idle.offer(new int[]{servers[i], i});
+        }
+
+        int m = tasks.length;
+        int[] ans = new int[m];
+        for(int j = 0; j < m; ++j) {
+            int t = tasks[j];
+            while(!busy.isEmpty() && busy.peek()[0] <= j) {
+                int[] p = busy.poll();
+                idle.offer(new int[]{p[1],p[2]});
+            }
+            if(!idle.isEmpty()) {
+                int i = idle.poll()[1];
+                ans[j] = i;
+                busy.offer(new int[]{t+j, servers[i], i});
+            } else {
+                int[] p = busy.poll();
+                ans[j] = p[2];
+                busy.offer(new int[]{p[0]+t,p[1],p[2]});
+            }
+            
+        }
+        return ans;
+    }
+}
