60732489fe9905dbabb277c7daeadc2c2a5cce4e
dean <deanmeisong@gmail.com>
2025-07-13 00:03:02 +1000

    Time: 9 ms (79.96%), Space: 61.3 MB (77.48%) - LeetHub

--- 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.java
+++ 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.java
@@ -1,14 +1,14 @@
 class Solution {
     public long maxAlternatingSum(int[] nums) {
         int n = nums.length;
-        // int[] f = new int[n+1];
-        // int[] g = new int[n+1];
-        // f[0] = 0; g[0] = 0;
-        // for(int i = 1; i <= n; ++i) {
-        //     f[i] = Math.max(g[i-1]-nums[i-1],f[i-1]);
-        //     g[i] = Math.max(f[i-1]+nums[i-1],g[i-1]);
-        // }
-        // return Math.max(f[n],g[n]);
+        long[] f = new long[n+1];
+        long[] g = new long[n+1];
+        f[0] = 0; g[0] = 0;
+        for(int i = 1; i <= n; ++i) {
+            f[i] = Math.max(g[i-1]-nums[i-1],f[i-1]);
+            g[i] = Math.max(f[i-1]+nums[i-1],g[i-1]);
+        }
+        return Math.max(f[n],g[n]);
 
         // int f = 0, g = 0;
         // for(int i = 0; i < n; ++i) {
@@ -17,14 +17,6 @@ class Solution {
         //     g = Math.max(ff+nums[i],gg);
         // }
         // return Math.max(f, g);
-        long ret = 0;
-        int i = 0;
-        while(i < nums.length) {
-            if(i+1 < nums.length && nums[i] > nums[i+1])
-                ret += nums[i]-nums[i+1];
-            ++i;
-        }
-        ret += nums[nums.length-1];
-        return ret;
+   
     }
 }
